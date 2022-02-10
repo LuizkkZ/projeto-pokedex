fbbace1cdc2eb920c2393ae1617e6c3147ba4edc
oliveira.bruno <bruno.yuri.alves.oliveira@outlook.com>
2022-02-10 11:11:29 -0300

    feat: Desenvolvido crud inicial para cadastro de legendaries

--- pokedexMVC/controllers/LegendariesController.js
+++ pokedexMVC/controllers/LegendariesController.js
@@ -3,6 +3,7 @@ const LegendaryModel = require('../models/LegendaryModel');
 const controller = {
   index: (request, response) => {
     const pokemon = new LegendaryModel(1, 'Pokemon Zika','Morador da ZL', 'Maloca')
+    return response.json(pokemon);
   }
 }
 

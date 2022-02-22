762dbe6b2fdff7a7180b2f58a72b822483aa026a
oliveira.bruno <bruno.yuri.alves.oliveira@outlook.com>
2022-02-22 08:45:42 -0300

    feat: Estruturando pastas e arquivos do projeto para implementacao do EJS

--- routes/legendaries.js
+++ routes/legendaries.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/LegendariesController');
 
+/* GET legendaries. */
 router.get('/', controller.index);
 
-
-module.exports = router
+module.exports = router;
